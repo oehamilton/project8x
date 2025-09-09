78e1916de1f4161d4b5dc86bdfa5601812531ed6
oehamilton <oehamilton@hotmail.com>
2025-09-09 11:21:06 -0500

    Build Update

--- project8x-website/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ project8x-website/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "project8xwebsite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
